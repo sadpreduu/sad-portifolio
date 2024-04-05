a01cb6294534693825360a58188b9c450236eaac
Pedro Carvalho <90916013+sadpreduu@users.noreply.github.com>
2024-04-05 16:31:42 -0300

    initial commit

---
+++ src/pages/components/HeroSection.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const HeroSection = () => {
+    return (
+        <section>
+            <h1 className="text-white mb-4 text-6xl lg:text-8xl font-extrabold">
+                Olá, sou Pedro Carvalho
+            </h1>
+            <p className="text-[#ADB7BE] text-lg lg:text-xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Fusce nibh felis, euismod vitae nunc at, viverra consequat mauris. 
+            Ut sagittis tincidunt orci, vel molestie purus dignissim in. 
+            Phasellus aliquam fermentum ultricies. Nam massa tellus, 
+            laoreet sit amet ante id, commodo pretium mauris. 
+            Nam vitae dolor ultrices, consequat metus sit amet, 
+            fringilla risus. Morbi non sollicitudin ex. Quisque pretium lectus eget tristique bibendum.
+            </p>
+        </section>
+    );
+};
+
+export default HeroSection;
