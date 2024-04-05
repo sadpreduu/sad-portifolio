b3aa05f711027c9ea04044a070e975148eae1e8a
Pedro Carvalho <90916013+sadpreduu@users.noreply.github.com>
2024-04-05 18:04:35 -0300

    feat: font added to the tailwind enviroment and placed on the hero page title

--- src/pages/components/HeroSection.jsx
+++ src/pages/components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
         <section>
             <div className="grid grid-cols-1 sm:grid-cols-12">
                 <div className="col-span-7 place-self-center text-center sm:text-left">
-                  <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                  <h1 className="font-RubikScribble text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
                     Olá, sou Pedro Carvalho
                   </h1>
                   <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
