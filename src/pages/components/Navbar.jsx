f39a09b6408838a02b1f7caa599aec39bbc25fd4
Pedro Carvalho <90916013+sadpreduu@users.noreply.github.com>
2024-04-08 15:10:30 -0300

    feat: navbar links added

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -9,7 +9,13 @@ const Navbar = () => {
            <Link href={"/"} className="text-5xl text-white font-semibold self-center h-3">
             SADBOY
            </Link>
-           <div className="menu"></div>
+           <div className="menu hidden md:block md:w-auto" id="navbar">
+            <ul>
+                <li><Link href={"#about"}className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl roudend md:p-0 hover:text-white">Sobre</Link></li>
+                <li><Link href={"#projects"}>Projetos</Link></li>
+                <li><Link href={"#contact"}>Contato</Link></li>
+            </ul>
+           </div>
         </div>
     </nav>
   )
