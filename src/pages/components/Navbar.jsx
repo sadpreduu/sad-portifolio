77dca71edcf9151b70090a868af5e1fb726dbbd1
Pedro Carvalho <90916013+sadpreduu@users.noreply.github.com>
2024-04-11 17:50:32 -0300

    feat: created new component

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -7,13 +7,16 @@ const Navbar = () => {
     <nav>
         <div className="flex flex-wrap items-centefr justify-between mx-auto p-8">
            <Link href={"/"} className="text-5xl text-white font-semibold self-center h-3">
-            SADBOY
+            PEDRO CARVALHO
            </Link>
            <div className="menu hidden md:block md:w-auto" id="navbar">
             <ul>
-                <li><Link href={"#about"}className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl roudend md:p-0 hover:text-white">Sobre</Link></li>
-                <li><Link href={"#projects"}>Projetos</Link></li>
-                <li><Link href={"#contact"}>Contato</Link></li>
+                <li><Link href={"#about"}className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl roudend md:p-0 hover:text-white">
+                  Sobre</Link></li>
+                <li><Link href={"#projects"}className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl roudend md:p-0 hover:text-white">
+                  Projetos</Link></li>
+                <li><Link href={"#contact"}className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl roudend md:p-0 hover:text-white">
+                  Contato</Link></li>
             </ul>
            </div>
         </div>
