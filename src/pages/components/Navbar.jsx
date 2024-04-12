17832cd5b042d648bb9cba2dbd86a8faeced1ee4
Pedro Carvalho <90916013+sadpreduu@users.noreply.github.com>
2024-04-12 17:53:34 -0300

    feat: added a flexbox to re align the menu navbar

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -23,12 +23,13 @@ const Navbar = () => {
   return (
     <nav>
         <div className="flex flex-wrap items-centefr justify-between mx-auto p-8">
-           <Link href={"/"} className="text-5xl text-white font-semibold self-center h-3">
+           <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold self-center h-3">
             PEDRO CARVALHO
            </Link>
            <div className="menu hidden md:block md:w-auto" id="navbar">
-            <ul>
-                {navLinks.map((link, index) => (
+            <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+                {
+                navLinks.map((link, index) => (
                     <li key={index}>
                         <NavLink href={link.path} title={link.title}/>
                     </li>
