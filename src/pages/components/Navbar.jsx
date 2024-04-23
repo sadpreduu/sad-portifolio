64d88e0dd6b42e7e4095f7380099877f5c46b5fc
Pedro Carvalho <90916013+sadpreduu@users.noreply.github.com>
2024-04-23 18:46:01 -0300

    feat: implementing the icons for the mobile view form.

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import Link from "next/link"
-import React from 'react'
-import NavLink from "./NavLink"
+"use client"; 
+import Link from "next/link";
+import React, { useState } from 'react';
+import NavLink from "./NavLink";
+import { Bars3Icon, XmarkIcon } from "@heroicons/react/24/solid";
 
 
 const navLinks = [
@@ -20,12 +22,27 @@ const navLinks = [
 
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false); 
+  
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
         <div className="flex flex-wrap items-centefr justify-between mx-auto px-4">
            <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold self-center h-3">
             PEDRO CARVALHO
            </Link>
+           <div className="mobile-menu block md:hidden">
+            {
+              navbarOpen ? (
+                <button className="text-slate-200">
+                  <Bars3Icon className="h-5 w-5"/>
+                </button>
+              ) : (  
+                <button className="text-slate-200">
+                  <XmarkIcon className="h-5 w-5"/>
+                </button>
+              )
+            }
+           </div>
            <div className="menu hidden md:block md:w-auto" id="navbar">
             <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
                 {
