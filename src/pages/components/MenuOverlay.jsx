86ebba6b56954792935fdf60668d21b136e52093
Pedro Carvalho <90916013+sadpreduu@users.noreply.github.com>
2024-05-28 10:15:08 -0300

    deploy: test 6, removed dist and re-write files with errors

--- src/pages/components/MenuOverlay.jsx
+++ src/pages/components/MenuOverlay.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import NavLink from "./NavLink";
 
 const MenuOverlay = ({ links }) => {
+  // Verifique se links está definido e é um array
+  if (!Array.isArray(links)) {
+    return null; // Ou exiba um fallback apropriado
+  }
+
   return (
     <ul className="flex flex-col py-4 items-center">
       {links.map((link, index) => (
