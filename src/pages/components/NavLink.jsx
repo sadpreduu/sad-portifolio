32b6f9d69e456fa4b8edb9cf87be16fd29d0365b
Pedro Carvalho <90916013+sadpreduu@users.noreply.github.com>
2024-05-28 12:08:25 -0300

    fix: Update NavLink.jsx

--- src/pages/components/NavLink.jsx
+++ src/pages/components/NavLink.jsx
@@ -1,11 +1,18 @@
-import Link from "next/link"
+import Link from "next/link";
 
 const NavLink = ({ href, title }) => {
-    return (
-    <Link href={href}
-        className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl roudend md:p-0 hover:text-white">
+  // Verifique se href e title estão definidos
+  if (!href || !title) {
+    return null; // Ou exiba um fallback apropriado
+  }
+
+  return (
+    <Link href={href}>
+      <a className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl roudend md:p-0 hover:text-white">
         {title}
-    </Link>);
+      </a>
+    </Link>
+  );
 };
 
 export default NavLink;
